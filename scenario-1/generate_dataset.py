1ca37976898d00d52e297d5fd74d8120131311af
Aidan Scannell <scannell.aidan@gmail.com>
2021-01-15 15:27:24 +0000

    Changed script to used env_config.toml and new parser

--- scenario-1/generate_dataset.py
+++ scenario-1/generate_dataset.py
@@ -1,19 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from simenvs.generate_dataset_from_env import generate_transitions_dataset
+from simenvs.parse_env_toml_config import \
+    parse_toml_config_to_VelocityControlledQuadcopter2DEnv
 
-num_data_per_dim = 40
-num_actions_per_dim = 4
-save_dataset_filename = "./data/quad_sim_scenario_1.npz"
-gating_bitmap = "./gating_mask.bmp"
-omit_data_mask = "./omit_data_mask.bmp"
+toml_env_config_file = "./configs/env_config.toml"  # environment config to use
+omit_data_mask = "./bitmaps/omit_data_mask.bmp"  # remove states if bmp<255/2
+save_dataset_filename = "./data/quad_sim_scenario_1.npz"  # save dataset here
 
-state_action_inputs, delta_state_outputs = generate_transitions_dataset(
-    gating_bitmap,
-    omit_data_mask=omit_data_mask,
-    num_data_per_dim=num_data_per_dim,
-    num_actions_per_dim=num_actions_per_dim,
+num_states = 1000  # number of states to randomly generate
+num_actions = 4  # number of actions to randomly generate
+
+# configure environment from toml config file
+env = parse_toml_config_to_VelocityControlledQuadcopter2DEnv(
+    toml_env_config_file
 )
+
+(
+    state_action_inputs,
+    delta_state_outputs,
+) = generate_transitions_dataset(
+    num_states=num_states, num_actions=num_actions, env=env, omit_data_mask=omit_data_mask
+)
+
 np.savez(
     save_dataset_filename,
     x=state_action_inputs,
