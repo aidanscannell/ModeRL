a78e3637bbe2465eedc8ef3279c76e388b8d4d36
Aidan Scannell <scannell.aidan@gmail.com>
2021-09-24 16:53:49 +0100

    Commit of rollouts working with new VariationalPolicy

--- modeopt/rollouts.py
+++ modeopt/rollouts.py
@@ -8,27 +8,14 @@ import tensorflow as tf
 from gpflow import default_float
 from tensor_annotations import axes
 from tensor_annotations.axes import Batch
-from modeopt.policies import GaussianPolicy
+from modeopt.policies import VariationalPolicy
 
 StateDim = typing.NewType("StateDim", axes.Axis)
 ControlDim = typing.NewType("ControlDim", axes.Axis)
 
 
-def rollout_controller_in_dynamics(
-    controller,
-    start_state: ttf.Tensor2[Batch, StateDim],
-    start_state_var: ttf.Tensor2[Batch, StateDim] = None,
-):
-    rollout_policy_in_dynamics(
-        policy=controller.policy,
-        dynamics=controller.dynamics,
-        start_state=start_state,
-        start_state_var=start_state_var,
-    )
-
-
 def rollout_policy_in_dynamics(
-    policy: GaussianPolicy,
+    policy: VariationalPolicy,
     dynamics: Callable,
     start_state: ttf.Tensor2[Batch, StateDim],
     start_state_var: ttf.Tensor2[Batch, StateDim] = None,
@@ -54,25 +41,20 @@ def rollout_controls_in_dynamics(
 
     :returns: (states_means, state_vars)
     """
-    print("here")
-    print(start_state.shape)
-    num_time_steps = control_means.shape[0]
+    horizon = control_means.shape[0]
     if start_state_var is None:
         start_state_var = tf.zeros((1, start_state.shape[1]), dtype=default_float())
 
     state_means = start_state
     state_vars = start_state_var
     control_var = None
-    for t in range(num_time_steps):
+    for t in range(horizon):
         control_mean = control_means[t : t + 1, :]
         if control_vars is not None:
             control_var = control_vars[t : t + 1, :]
         next_state_mean, next_state_var = dynamics(
             state_means[-1:, :], control_mean, state_vars[-1:, :], control_var
         )
-        print("next_state_mean")
-        print(next_state_mean.shape)
-        print(next_state_var.shape)
         state_means = tf.concat([state_means, next_state_mean], 0)
         state_vars = tf.concat([state_vars, next_state_var], 0)
     return state_means, state_vars
