0b94c923c97cf7be016c604b4641da8c228a6819
Aidan Scannell <scannell.aidan@gmail.com>
2021-10-05 09:44:26 +0100

    added function to create a tf dataset for training

--- modeopt/dynamics/utils.py
+++ modeopt/dynamics/utils.py
@@ -41,3 +41,20 @@ def init_ModeOptDynamics_from_mogpe_ckpt(
         return dynamics, (X, Y)
     else:
         return dynamics
+
+
+def create_tf_dataset(dataset, batch_size):
+    X, Y = dataset
+    assert X.shape[0] == Y.shape[0]
+    num_data = X.shape[0]
+    prefetch_size = tf.data.experimental.AUTOTUNE
+    shuffle_buffer_size = num_data // 2
+    num_batches_per_epoch = num_data // batch_size
+    train_dataset = tf.data.Dataset.from_tensor_slices(dataset)
+    train_dataset = (
+        train_dataset.repeat()
+        .prefetch(prefetch_size)
+        .shuffle(buffer_size=shuffle_buffer_size)
+        .batch(batch_size, drop_remainder=True)
+    )
+    return train_dataset, num_batches_per_epoch
