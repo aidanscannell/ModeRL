4d027e3633f7787a39d007a8bd4e52c5a7c374e2
Aidan Scannell <scannell.aidan@gmail.com>
2021-10-19 23:22:17 +0100

    added ModeOptDynamicsTrainingSpec

--- modeopt/dynamics/__init__.py
+++ modeopt/dynamics/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from modeopt.dynamics.base import Dynamics
 from modeopt.dynamics.gp import SVGPDynamics
-from modeopt.dynamics.multimodal import ModeOptDynamics
+from modeopt.dynamics.multimodal import ModeOptDynamics, ModeOptDynamicsTrainingSpec
 
 GPDynamics = SVGPDynamics
