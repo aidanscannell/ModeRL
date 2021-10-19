367556bdae1e69b2dc78e19bc21c3f59326c5a30
Aidan Scannell <scannell.aidan@gmail.com>
2021-10-19 23:09:00 +0100

    added Riemmanian energy cost function
    Also added functions to build it from a gp and to build a quadratic cost
    for state/action/energy

--- modeopt/cost_functions.py
+++ modeopt/cost_functions.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-from typing import NewType, Optional, Union
+from functools import partial
+from typing import Callable, NewType, Optional, Union
 
 import tensor_annotations.tensorflow as ttf
 import tensorflow as tf
+from geoflow.manifolds import GPManifold
+from gpflow import default_float
+from gpflow.models import GPModel
 from tensor_annotations import axes
 from tensor_annotations.axes import Batch
 
@@ -12,6 +16,7 @@ StateDim = NewType("StateDim", axes.Axis)
 ControlDim = NewType("ControlDim", axes.Axis)
 InputDim = Union[StateDim, ControlDim]
 One = NewType("One", axes.Axis)
+Trajectory = NewType("Trajectory", axes.Axis)
 
 
 def quadratic_cost_fn(
@@ -56,6 +61,95 @@ def terminal_state_cost_fn(
     error = state - target_state
     terminal_cost = quadratic_cost_fn(error, Q, state_var)
     return terminal_cost
+
+
+def build_riemmanian_energy_cost_fn(
+    gp: GPModel,
+    riemmanian_metric_cost_weight: float = 1.0,
+    covariance_weight: float = 1.0,
+) -> Callable:
+    manifold = GPManifold(gp, covariance_weight=covariance_weight)
+    return partial(
+        riemmanian_energy_cost_fn,
+        manifold=manifold,
+        riemmanian_metric_cost_weight=riemmanian_metric_cost_weight,
+    )
+
+
+def riemmanian_energy_cost_fn(
+    state_trajectory: ttf.Tensor2[Trajectory, StateDim],
+    control_trajectory: ttf.Tensor2[Trajectory, ControlDim],
+    manifold: GPManifold,
+    riemmanian_metric_cost_weight: float = 1.0,
+    state_trajectory_var: Optional[ttf.Tensor2[Trajectory, StateDim]] = None,
+    control_trajectory_var: Optional[ttf.Tensor2[Trajectory, ControlDim]] = None,
+):
+    # Calcualted the expeted metric at each point along trajectory
+    input_mean = tf.concat([state_trajectory, control_trajectory], -1)
+    expected_riemannian_metric = (
+        manifold.metric(input_mean[1:, :])
+        * riemmanian_metric_cost_weight
+        # manifold.metric(input_mean[:-1 :]) * riemmanian_metric_cost_weight
+    )
+
+    velocities = input_mean[1:, :] - input_mean[:-1, :]
+    velocities_var = None
+
+    if state_trajectory_var is not None and control_trajectory_var is not None:
+        input_var = tf.concat([state_trajectory_var, control_trajectory_var], -1)
+        velocities_var = input_var[1:, :]
+
+    riemannian_energy = quadratic_cost_fn(
+        vector=velocities,
+        weight_matrix=expected_riemannian_metric,
+        # vector_var=None,
+        vector_var=velocities_var,
+    )
+    riemannian_energy_sum = tf.reduce_sum(riemannian_energy)
+    return riemannian_energy_sum
+
+
+def build_state_control_riemannian_energy_quadratic_cost_fn(
+    Q: ttf.Tensor2[StateDim, StateDim],
+    R: ttf.Tensor2[ControlDim, ControlDim],
+    gp: GPModel,
+    riemannian_metric_cost_weight: default_float(),
+    riemannian_metric_covariance_weight: default_float(),
+):
+    """Build quadratic cost func with state, control and Riemannian energy
+
+    c(x(t),u(t)) = x(t)^T Q x(t) + u(t)^T R u(t) + \dot{v(t)}^T G(v(t)) \dot{v(t)}^T
+
+    where v = (x, u) and where G(v) is the Riemannian metric at v.
+    """
+    state_control_cost_fn = partial(state_control_quadratic_cost_fn, Q=Q, R=R)
+    riemannian_energy_cost_fn = build_riemmanian_energy_cost_fn(
+        gp=gp,
+        riemmanian_metric_cost_weight=riemannian_metric_cost_weight,
+        covariance_weight=riemannian_metric_covariance_weight,
+    )
+
+    def state_control_riemannian_energy_quadratic_cost_fn(
+        state, control, state_var, control_var
+    ):
+        state_control_cost = state_control_cost_fn(
+            state=state,
+            control=control,
+            state_var=state_var,
+            control_var=control_var,
+        )
+        state_control_cost_sum = tf.reduce_sum(state_control_cost)
+        riemannian_cost = riemannian_energy_cost_fn(
+            state_trajectory=state,
+            control_trajectory=control,
+            # state_trajectory_var=state_var,
+            # control_trajectory_var=control_var,
+            state_trajectory_var=None,
+            control_trajectory_var=None,
+        )
+        return state_control_cost_sum + riemannian_cost
+
+    return state_control_riemannian_energy_quadratic_cost_fn
 
 
 # def terminal_cost_fn(terminal, Q, target, terminal_var=None):
