14195856f7b6362ad90bffd250cbb565581505cf
Aidan Scannell <scannell.aidan@gmail.com>
2021-11-10 17:39:50 +0000

    created methods for plotting env/dynamics trajectoreis on same fig

--- modeopt/monitor.py
+++ modeopt/monitor.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
+import os
+
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import tensorflow as tf
-from mogpe.helpers import Plotter2D
-from mogpe.helpers.quadcopter_plotter import QuadcopterPlotter
 from gpflow.monitor import (
     ModelToTensorBoard,
     Monitor,
     MonitorTaskGroup,
     ScalarToTensorBoard,
 )
+from mogpe.helpers import Plotter2D
+from mogpe.helpers.quadcopter_plotter import (
+    QuadcopterPlotter,
+    init_axis_labels_and_ticks,
+)
 from mogpe.training.monitor import ImageWithCbarToTensorBoard
+
 from modeopt.mode_opt import ModeOpt
 
 plt.style.use("science")
+matplotlib.rcParams.update({"font.size": 10})
+
+expert_colors = ["c", "m", "y"]
+colors = {"initial": "c", "optimised": "m"}
+linestyles = {"initial": "x", "optimised": "*"}
+markers = {"initial": "x", "optimised": "*"}
+
+styles = {"env": "c-.", "dynamics": "m-s"}
+labels = {"env": "Environment", "dynamics": "Dynamics"}
+
+# svgp_linestyle = "--"
+# svgp_linestyle = "-"
 
 
 class ModeOptPlotter:
@@ -24,76 +43,307 @@ class ModeOptPlotter:
         mogpe_plotter: QuadcopterPlotter,
     ):
         self.mogpe_plotter = mogpe_plotter
+        self.figsize = self.mogpe_plotter.figsize
         self.mode_opt = mode_opt
+        self.desired_mode = self.mode_opt.desired_mode
         self.start_state = self.mode_opt.start_state
         self.target_state = self.mode_opt.target_state
-        # self.gp_means, self.gp_vars = self.mogpe_plotter.predict_y(
-        #     self.mogpe.plotter.test_inputs, full_cov=False
+
+    def plot_model(self, save_dir):
+        try:
+            os.makedirs(save_dir)
+        except:
+            print("save_dir already exists")
+        # save_filename = os.path.join(save_dir, "env_rollout_over_prob.pdf")
+        # self.plot_rollout_over_prob(save_filename=save_filename, dynamics=False)
+        # save_filename = os.path.join(save_dir, "env_rollout_over_desired_prob.pdf")
+        # self.plot_rollout_over_prob(
+        #     save_filename=save_filename, desired_mode=self.desired_mode
         # )
-        # print("gp_means")
-        # print(self.gp_means.shape)
-        # print(self.gp_vars.shape)
-        # self.h_means, self.h_vars = self.mode_opt.dynamics.gating_gp.predict_f(
-        #     self.mogpe.plotter.test_inputs, full_cov=False
+        # save_filename = os.path.join(save_dir, "env_rollout_over_gating_gps.pdf")
+        # self.plot_rollout_over_gating_gps(save_filename=save_filename, dynamics=False)
+        # save_filename = os.path.join(
+        #     save_dir, "env_rollout_over_desired_gating_gps.pdf"
         # )
-        # self.mode_prob = self.mode_opt.dynamics.gating_gp.predict_y(
-        #     self.mogpe.plotter.test_inputs
+        # self.plot_rollout_over_gating_gps(
+        #     save_filename=save_filename, desired_mode=self.desired_mode, dynamics=False
         # )
 
-    def plot_env_rollout_over_mode_prob(self, fig, axs):
+        # save_filename = os.path.join(save_dir, "dynamics_rollout_over_prob.pdf")
+        # self.plot_dynamics_rollout_over_prob(save_filename=save_filename)
+        # save_filename = os.path.join(save_dir, "dynamics_rollout_over_desired_prob.pdf")
+        # self.plot_dynamics_rollout_over_prob(
+        #     save_filename=save_filename, desired_mode=self.desired_mode
+        # )
+        # save_filename = os.path.join(
+        #     save_dir, "dynamics_rollout_over_desired_gating_gps.pdf"
+        # )
+        # self.plot_dynamics_rollout_over_gating_gps(
+        #     save_filename=save_filename, desired_mode=self.desired_mode
+        # )
+        # save_filename = os.path.join(save_dir, "dynamics_rollout_over_gating_gps.pdf")
+        # self.plot_dynamics_rollout_over_gating_gps(save_filename=save_filename)
+        # # save_filename = os.path.join(
+        # #     save_dir, "env_and_dynamics_rollout_over_gating_gps.pdf"
+        # # )
+        # # self.plot_env_and_dynamics_rollout_over_gating_gps(save_filename=save_filename)
+
+        save_filename = os.path.join(
+            save_dir, "env_and_dynamics_rollout_over_desired_prob.pdf"
+        )
+        self.plot_env_and_dynamics_rollout_over_prob(
+            save_filename=save_filename, desired_mode=self.desired_mode
+        )
+        save_filename = os.path.join(
+            save_dir, "env_and_dynamics_rollout_over_desired_gating_gps.pdf"
+        )
+        self.plot_env_and_dynamics_rollout_over_gating_gps(
+            save_filename=save_filename, desired_mode=self.desired_mode
+        )
+
+    def create_fig_axs_plot_desired_prob(self):
+        fig = plt.figure(figsize=(self.figsize[0] / 2, self.figsize[1] / 4))
+        gs = fig.add_gridspec(1, 1, wspace=0.3)
+        axs = gs.subplots(sharex=True, sharey=True)
+        axs = init_axis_labels_and_ticks(axs)
+        return fig, axs
+
+    def create_fig_axs_plot_desired_gating_gps(self):
+        fig = plt.figure(figsize=(self.figsize[0], self.figsize[1] / 4))
+        gs = fig.add_gridspec(1, 2, wspace=0.3)
+        axs = gs.subplots(sharex=True, sharey=True)
+        axs = init_axis_labels_and_ticks(axs)
+        return fig, axs
+
+    def plot_rollout_over_prob(
+        self, save_filename=None, dynamics=True, desired_mode=None
+    ):
+        if desired_mode is None:
+            fig, axs = self.mogpe_plotter.create_fig_axs_plot_prob()
+        else:
+            fig, axs = self.create_fig_axs_plot_desired_prob()
+        if dynamics:
+            self.plot_dynamics_rollout_over_prob_given_fig_axs(
+                fig, axs, desired_mode=desired_mode
+            )
+        else:
+            self.plot_env_rollout_over_prob_given_fig_axs(
+                fig, axs, desired_mode=desired_mode
+            )
+        if save_filename is not None:
+            plt.savefig(save_filename, transparent=True)
+
+    def plot_rollout_over_gating_gps(
+        self, save_filename=None, dynamics=True, desired_mode=None
+    ):
+        if desired_mode is None:
+            fig, axs = self.mogpe_plotter.create_fig_axs_plot_gating_gps()
+        else:
+            fig, axs = self.create_fig_axs_plot_desired_gating_gps()
+        if dynamics:
+            self.plot_dynamics_rollout_over_gating_gps_given_fig_axs(
+                fig, axs, desired_mode=desired_mode
+            )
+        else:
+            self.plot_env_rollout_over_gating_gps_given_fig_axs(
+                fig, axs, desired_mode=desired_mode
+            )
+        if save_filename is not None:
+            plt.savefig(save_filename, transparent=True)
+
+    def plot_env_and_dynamics_rollout_over_gating_gps(
+        self, save_filename=None, desired_mode=None
+    ):
+        fig, axs = self.create_fig_axs_plot_desired_gating_gps()
+        self.plot_env_rollout_over_gating_gps_given_fig_axs(
+            fig, axs, desired_mode=desired_mode
+        )
+        self.plot_dynamics_rollout_over_gating_gps_given_fig_axs(
+            fig, axs, desired_mode=desired_mode
+        )
+        if save_filename is not None:
+            plt.savefig(save_filename, transparent=True)
+
+    def plot_env_and_dynamics_rollout_over_prob(
+        self, save_filename=None, desired_mode=None
+    ):
+        if desired_mode is None:
+            fig, axs = self.mogpe_plotter.create_fig_axs_plot_prob()
+        else:
+            fig, axs = self.create_fig_axs_plot_desired_prob()
+        self.plot_env_rollout_over_prob_given_fig_axs(
+            fig, axs, desired_mode=desired_mode
+        )
+        self.plot_dynamics_rollout_over_prob_given_fig_axs(
+            fig, axs, desired_mode=desired_mode
+        )
+        if save_filename is not None:
+            plt.savefig(save_filename, transparent=True)
+
+    def plot_env_rollout_over_prob_given_fig_axs(self, fig, axs, desired_mode=None):
         trajectory = self.mode_opt.env_rollout(self.start_state)
-        return self.plot_trajectory_over_mode_prob(fig, axs, trajectory)
+        return self.plot_trajectory_over_prob(
+            fig,
+            axs,
+            trajectory,
+            desired_mode=desired_mode,
+            label="env",
+        )
+        # cbars = []
+        # if isinstance(axs, np.ndarray):
+        #     for ax in axs.flat:
+        #         cbars.append(
+        #             self.plot_trajectory_over_prob(
+        #                 fig,
+        #                 ax,
+        #                 trajectory,
+        #                 desired_mode=desired_mode,
+        #                 label="env",
+        #             )
+        #         )
+        # else:
+        #     cbars.append(
+        #         self.plot_trajectory_over_prob(
+        #             fig, axs, trajectory, desired_mode=desired_mode, label="env"
+        #         )
+        #     )
+        # return cbars
 
-    def plot_env_rollout_over_gating_gps(self, fig, axs):
+    def plot_env_rollout_over_gating_gps_given_fig_axs(
+        self, fig, axs, desired_mode=None
+    ):
         trajectory = self.mode_opt.env_rollout(self.start_state)
-        return self.plot_trajectory_over_gating_gps(fig, axs, trajectory)
+        return self.plot_trajectory_over_gating_gps(
+            fig, axs, trajectory, desired_mode=desired_mode, label="env"
+        )
 
-    def plot_dynamics_rollout_over_mode_prob(self, fig, axs):
+    def plot_dynamics_rollout_over_prob_given_fig_axs(
+        self, fig, axs, desired_mode=None
+    ):
         state_means, state_vars = self.mode_opt.dynamics_rollout(
             self.start_state, start_state_var=None
         )
-        return self.plot_trajectory_over_mode_prob(fig, axs, trajectory=state_means)
+        return self.plot_trajectory_over_prob(
+            fig,
+            axs,
+            trajectory=state_means,
+            desired_mode=desired_mode,
+            label="dynamics",
+        )
+        # cbars = []
+        # if isinstance(axs, np.ndarray):
+        #     for ax in axs.flat:
+        #         cbars.append(
+        #             self.plot_trajectory_over_prob(
+        #                 fig,
+        #                 ax,
+        #                 trajectory=state_means,
+        #                 desired_mode=desired_mode,
+        #                 label="dynamics",
+        #             )
+        #         )
+        # else:
+        #     cbars.append(
+        #         self.plot_trajectory_over_prob(
+        #             fig,
+        #             axs,
+        #             trajectory=state_means,
+        #             desired_mode=desired_mode,
+        #             label="dynamics",
+        #         )
+        #     )
+        # return cbars
 
-    def plot_dynamics_rollout_over_gating_gps(self, fig, axs):
+    def plot_dynamics_rollout_over_gating_gps_given_fig_axs(
+        self, fig, axs, desired_mode=None
+    ):
         state_means, state_vars = self.mode_opt.dynamics_rollout(
             self.start_state, start_state_var=None
         )
-        return self.plot_trajectory_over_gating_gps(fig, axs, trajectory=state_means)
+        return self.plot_trajectory_over_gating_gps(
+            fig,
+            axs,
+            trajectory=state_means,
+            desired_mode=desired_mode,
+            label="dynamics",
+        )
 
-    def plot_trajectory_over_gating_gps(self, fig, axs, trajectory):
+    def plot_trajectory_over_gating_gps(
+        self, fig, axs, trajectory, desired_mode=None, label=None
+    ):
         # cbars = self.mogpe_plotter.plot_gating_gps(fig, axs)
-        cbars = self.mogpe_plotter.plot_gating_gps_given_fig_axs(fig, axs)
-        self.plot_trajectory_over_axs(fig, axs, trajectory)
+        cbars = self.mogpe_plotter.plot_gating_gps_given_fig_axs(
+            fig, axs, desired_mode=desired_mode
+        )
+        self.plot_trajectory_over_axs(fig, axs, trajectory, label=label)
         return cbars
 
-    def plot_trajectory_over_mode_prob(self, fig, axs, trajectory):
+    def plot_trajectory_over_prob(
+        self, fig, axs, trajectory, desired_mode=None, label=None
+    ):
         # cbars = self.mogpe_plotter.plot_gating_network(fig, axs)
-        cbars = self.mogpe_plotter.plot_mixing_probs_given_fig_axs(fig, axs)
-        self.plot_trajectory_over_axs(fig, axs, trajectory)
+        cbars = self.mogpe_plotter.plot_mixing_probs_given_fig_axs(
+            fig, axs, desired_mode=desired_mode
+        )
+        self.plot_trajectory_over_axs(fig, axs, trajectory, label=label)
         return cbars
 
-    def plot_trajectory_over_axs(self, fig, axs, trajectory, color="k"):
-        for ax in axs.flat:
+    def plot_trajectory_over_axs(self, fig, axs, trajectory, label=None):
+        def plot_fn(ax):
             # ax.scatter(trajectory[:, 0], trajectory[:, 1], marker=".", color=color)
-            ax.plot(trajectory[:, 0], trajectory[:, 1], marker=".", color=color)
-            ax.scatter(
-                self.start_state[0, 0], self.start_state[0, 1], marker="x", color="k"
+            # , label=labels["env"]
+            # ax.plot(trajectory[:, 0], trajectory[:, 1], marker=".", color=color)
+            if label is None:
+                style = ""
+                traj_label = ""
+            else:
+                style = styles[label]
+                traj_label = labels[label]
+            ax.plot(
+                trajectory[:, 0],
+                trajectory[:, 1],
+                style,
+                label=traj_label,
+                markersize=2.0,
             )
             ax.scatter(
-                self.target_state[0, 0], self.target_state[0, 1], color="k", marker="x"
+                self.start_state[0, 0],
+                self.start_state[0, 1],
+                marker="x",
+                color="k",
+                s=8.0,
+            )
+            ax.scatter(
+                self.target_state[0, 0],
+                self.target_state[0, 1],
+                color="k",
+                marker="x",
+                s=8.0,
             )
             ax.annotate(
-                "Start $\mathbf{x}_0$",
+                # "Start $\mathbf{x}_0$",
+                "$\mathbf{x}_0$",
                 (self.start_state[0, 0], self.start_state[0, 1]),
                 horizontalalignment="left",
                 verticalalignment="top",
             )
             ax.annotate(
-                "End $\mathbf{x}_f$",
+                # "End $\mathbf{x}_f$",
+                "$\mathbf{x}_f$",
                 (self.target_state[0, 0], self.target_state[0, 1]),
+                # horizontalalignment="left",
+                # verticalalignment="top",
                 horizontalalignment="left",
-                verticalalignment="top",
+                verticalalignment="bottom",
             )
+
+        if isinstance(axs, np.ndarray):
+            for ax in axs.flat:
+                plot_fn(ax)
+            axs.flatten()[-1].legend()
+        else:
+            plot_fn(axs)
+            axs.legend()
 
     def tf_monitor_task_group(self, log_dir, slow_tasks_period=500):
         # figsize = self.mogpe_plotter.figsize
@@ -101,7 +351,7 @@ class ModeOptPlotter:
         num_experts = self.mogpe_plotter.num_experts
         image_task_dynamics_traj = ImageWithCbarToTensorBoard(
             log_dir,
-            self.plot_dynamics_rollout_over_gating_gps,
+            self.plot_dynamics_rollout_over_gating_gps_given_fig_axs,
             name="dynamics_rollout_over_dynamics_gp",
             fig_kw={"figsize": (figsize[0], figsize[1] / 4)},
             # subplots_kw={"nrows": 1, "ncols": 2},
@@ -110,7 +360,7 @@ class ModeOptPlotter:
         )
         image_task_env_traj = ImageWithCbarToTensorBoard(
             log_dir,
-            self.plot_env_rollout_over_gating_gps,
+            self.plot_env_rollout_over_gating_gps_given_fig_axs,
             name="env_rollout_over_dynamics_gp",
             fig_kw={"figsize": (figsize[0], figsize[1] / 2)},
             # subplots_kw={"nrows": 2, "ncols": 2},
@@ -119,8 +369,8 @@ class ModeOptPlotter:
         )
         image_task_dynamics_traj_prob = ImageWithCbarToTensorBoard(
             log_dir,
-            self.plot_dynamics_rollout_over_mode_prob,
-            name="dynamics_rollout_over_mode_prob",
+            self.plot_dynamics_rollout_over_prob_given_fig_axs,
+            name="dynamics_rollout_over_prob",
             fig_kw={"figsize": (figsize[0], figsize[1] / 4)},
             # fig_kw={"figsize": (10, 4)},
             # subplots_kw={"nrows": 1, "ncols": 2},
@@ -130,8 +380,8 @@ class ModeOptPlotter:
         )
         image_task_env_traj_prob = ImageWithCbarToTensorBoard(
             log_dir,
-            self.plot_env_rollout_over_mode_prob,
-            name="env_rollout_over_mode_prob",
+            self.plot_env_rollout_over_prob_given_fig_axs,
+            name="env_rollout_over_prob",
             fig_kw={"figsize": (figsize[0], figsize[1] / 4)},
             # fig_kw={"figsize": (10, 4)},
             # subplots_kw={"nrows": 1, "ncols": 2},
