db86d2ec746275e73b4651e69c3abffbb4e05d9a
Aidan Scannell <scannell.aidan@gmail.com>
2021-11-15 11:58:42 +0000

    dceleted unused imports

--- examples/velocity_controlled_point_mass/scenario_4/run_mode_opt_remain.py
+++ examples/velocity_controlled_point_mass/scenario_4/run_mode_opt_remain.py
@@ -1,33 +1,20 @@
 #!/usr/bin/env python3
-import os
-from datetime import datetime
-
 import gin.tf
-import numpy as np
-from gpflow import default_float
-from modeopt.monitor import init_ModeOpt_monitor
-from modeopt.trajectory_optimisers import (
-    ModeVariationalTrajectoryOptimiserTrainingSpec,
-    VariationalTrajectoryOptimiserTrainingSpec,
-)
 
-from scenario_4.utils import (
-    init_checkpoint_manager,
-    init_mode_opt,
-    config_traj_opt,
-    velocity_controlled_point_mass_dynamics,
-)
-
+from scenario_4.utils import config_traj_opt
 
 if __name__ == "__main__":
-    mode_opt_config = (
+    mode_opt_config_file = (
         # "./scenario_4/configs/mode_remaining_traj_opt_config.gin",
         # "./scenario_4/configs/mode_remaining_riemannian_energy_traj_opt_config.gin",
+        # "./scenario_4/configs/mode_remaining_conditioning_energy_traj_opt_config.gin",
         # "./scenario_4/configs/mode_remaining_chance_constraints_traj_opt_config.gin",
         "./scenario_4/configs/mode_remaining_mode_conditioning_traj_opt_config.gin",
         # "./configs/mode_remaining_traj_opt_config.gin",
     )
-    gin.parse_config_files_and_bindings([mode_opt_config[0]], None)
-    mode_optimiser, training_spec = config_traj_opt(mode_opt_config=mode_opt_config[0])
+    gin.parse_config_files_and_bindings([mode_opt_config_file[0]], None)
+    mode_optimiser, training_spec = config_traj_opt(
+        mode_opt_config_file=mode_opt_config_file[0]
+    )
 
     mode_optimiser.optimise_policy(mode_optimiser.start_state, training_spec)
