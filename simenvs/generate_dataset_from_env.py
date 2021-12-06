654ad9a5c2bbce13a2428fe60e4d6c75610692b0
Aidan Scannell <scannell.aidan@gmail.com>
2021-12-06 09:15:33 +0000

    corrected state/control dim to change for each env

--- simenvs/generate_dataset_from_env.py
+++ simenvs/generate_dataset_from_env.py
@@ -3,7 +3,7 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 
-from simenvs.quadcopter_sim import VelocityControlledQuadcopter2DEnv
+# from simenvs.point_mass_sim import PointMass2DEnv
 
 float_type = np.float64
 
@@ -79,9 +79,7 @@ def apply_mask_to_states(states, env, omit_data_mask=None):
     elif isinstance(omit_data_mask, np.ndarray):
         omit_data_mask = omit_data_mask
     else:
-        raise (
-            "omit_data_mask must be np.ndarray or filepath string for bitmap"
-        )
+        raise ("omit_data_mask must be np.ndarray or filepath string for bitmap")
 
     rows_to_delete = []
     for row in range(states.shape[0]):
@@ -100,6 +98,7 @@ def create_state_action_inputs(states, actions):
     :param actions: [num_actions, action_dim]
     :returns: state-action inputs [num_states*num_actions, state_dim+action_dim]
     """
+
     def grid_action(action):
         action = action.reshape(1, -1)
         num_test = states.shape[0]
@@ -117,7 +116,7 @@ def create_state_action_inputs(states, actions):
     return state_action_inputs
 
 
-def transition_dynamics(state_action, env):
+def transition_dynamics(state_action, env, num_states):
     """Wrapper for calling env.transition_dynamics(state, action) with state_action
 
     :param state_action: state-action tuple [state_dim+action_dim]
@@ -125,16 +124,14 @@ def transition_dynamics(state_action, env):
     :returns: delta_state [state_dim]
     """
     state_action = state_action.reshape(1, -1)
-    num_dims = int(state_action.shape[1] / 2)
-    state = state_action[:, :num_dims]
-    action = state_action[:, num_dims:]
+    # num_dims = int(state_action.shape[1] / 2)
+    state = state_action[:, :num_states]
+    action = state_action[:, num_states:]
     delta_state = env.transition_dynamics(state, action)
     return delta_state.reshape(-1)
 
 
-def generate_transitions_dataset(
-    num_states, num_actions, env, omit_data_mask=None
-):
+def generate_transitions_dataset(num_states, num_actions, env, omit_data_mask=None):
     """Generate dataset of state transitions
 
     :param num_states: number of states to randomly generate
@@ -145,7 +142,8 @@ def generate_transitions_dataset(
               delta_state_outputs [num_states*num_actions, state_dim]
     """
     # generate states
-    state_dim = env.observation_spec().shape[1]
+    # state_dim = env.observation_spec().shape[1]
+    state_dim = env.observation_spec().shape[0]
     min_state = env.observation_spec().minimum
     max_state = env.observation_spec().maximum
     states = generate_random_states(
@@ -159,7 +157,9 @@ def generate_transitions_dataset(
     print("Initial states shape after applying mask: ", states.shape)
 
     # generate actions
-    action_dim = env.action_spec().shape[1]
+    # action_dim = env.action_spec().shape[1]
+    action_dim = env.action_spec().shape[0]
+    num_actions = num_states.shape[0]
     min_action = env.action_spec().minimum
     max_action = env.action_spec().maximum
     actions = generate_random_actions(
@@ -178,7 +178,9 @@ def generate_transitions_dataset(
     num_data = state_action_inputs.shape[0]
     delta_state_outputs = []
     for row in range(num_data):
-        delta_state = transition_dynamics(state_action_inputs[row, :], env)
+        delta_state = transition_dynamics(
+            state_action_inputs[row, :], env, num_states=state_dim
+        )
         delta_state_outputs.append(delta_state)
     delta_state_outputs = np.stack(delta_state_outputs)
 
@@ -200,6 +202,7 @@ def generate_transitions_dataset_const_action(
     """
     # generate states
     state_dim = env.observation_spec().shape[1]
+    # state_dim = env.observation_spec().shape[0]
     min_state = env.observation_spec().minimum
     max_state = env.observation_spec().maximum
     states = generate_random_states(
@@ -225,7 +228,9 @@ def generate_transitions_dataset_const_action(
     num_data = state_action_inputs.shape[0]
     delta_state_outputs = []
     for row in range(num_data):
-        delta_state = transition_dynamics(state_action_inputs[row, :], env)
+        delta_state = transition_dynamics(
+            state_action_inputs[row, :], env, num_states=state_dim
+        )
         delta_state_outputs.append(delta_state)
     delta_state_outputs = np.stack(delta_state_outputs)
 
@@ -233,60 +238,124 @@ def generate_transitions_dataset_const_action(
     return state_action_inputs, delta_state_outputs
 
 
-def test_generate_transitions_dataset_const_action():
-    num_states = 1000
-    action = np.array([0.1, 0.1])
-    # gating_bitmap = "../scenario-1/gating_mask.bmp"
-    # omit_data_mask = "../scenario-1/omit_data_mask.bmp"
-    gating_bitmap = "../scenario-2/gating_mask.bmp"
-    omit_data_mask = "../scenario-2/omit_data_mask.bmp"
-    env = VelocityControlledQuadcopter2DEnv(gating_bitmap=gating_bitmap)
+def generate_random_transitions_dataset(num_samples, env, omit_data_mask=None):
+    """Generate dataset of state transitions
 
-    (
-        state_action_inputs,
-        delta_state_outputs,
-    ) = generate_transitions_dataset_const_action(
-        action, num_states=num_states, env=env, omit_data_mask=omit_data_mask
+    :param num_states: number of samples to generate
+    :param env: an instance of VelocityControlledQuadcopter2DEnv
+    :param omit_data_mask: bitmaap used to omit data from dataset
+    :returns: state_action_inputs [num_states*num_actions, state_dim+action_dim]
+              delta_state_outputs [num_states*num_actions, state_dim]
+    """
+    # generate states
+    # state_dim = env.observation_spec().shape[1]
+    # state_dim = env.observation_spec().shape[0]
+    if len(env.observation_spec().shape) == 2:
+        state_dim = env.observation_spec().shape[1]
+    elif len(env.observation_spec().shape) == 1:
+        state_dim = env.observation_spec().shape[0]
+    min_state = env.observation_spec().minimum
+    max_state = env.observation_spec().maximum
+    states = generate_random_states(
+        state_dim,
+        num_states=num_samples,
+        min_state=min_state,
+        max_state=max_state,
     )
-    assert state_action_inputs.shape[0] == delta_state_outputs.shape[0]
-    assert state_action_inputs.shape[1] == 2 * delta_state_outputs.shape[1]
-    plt.quiver(
-        state_action_inputs[:, 0],
-        state_action_inputs[:, 1],
-        delta_state_outputs[:, 0],
-        delta_state_outputs[:, 1],
+    print("Initial states shape: ", states.shape)
+    states = apply_mask_to_states(states, env, omit_data_mask)
+    print("Initial states shape after applying mask: ", states.shape)
+
+    # generate actions
+    # action_dim = env.action_spec().shape[1]
+    # action_dim = env.action_spec().shape[0]
+    if len(env.action_spec().shape) == 2:
+        action_dim = env.action_spec().shape[1]
+    elif len(env.action_spec().shape) == 1:
+        action_dim = env.action_spec().shape[0]
+    num_samples = states.shape[0]
+    min_action = env.action_spec().minimum
+    max_action = env.action_spec().maximum
+    actions = generate_random_actions(
+        action_dim,
+        num_actions=num_samples,
+        min_action=min_action,
+        max_action=max_action,
     )
-    plt.show()
+    print("Initial actions shape: ", actions.shape)
+
+    # create every combination of states and actions to get inputs
+    # state_action_inputs = create_state_action_inputs(states, actions)
+    state_action_inputs = jnp.concatenate([states, actions], -1)
+    print("State action inputs shape: ", state_action_inputs.shape)
+    # print(state_action_inputs)
+
+    delta_state_outputs = []
+    for row in range(num_samples):
+        delta_state = transition_dynamics(
+            state_action_inputs[row, :], env, num_states=state_dim
+        )
+        delta_state_outputs.append(delta_state)
+    delta_state_outputs = np.stack(delta_state_outputs)
+
+    print("Delta state outputs: ", delta_state_outputs.shape)
+    return state_action_inputs, delta_state_outputs
 
 
-def test_generate_transitions_dataset():
-    num_states = 100
-    num_actions = 16
-    # gating_bitmap = "../scenario-1/gating_mask.bmp"
-    # omit_data_mask = "../scenario-1/omit_data_mask.bmp"
-    gating_bitmap = "../scenario-2/gating_mask.bmp"
-    omit_data_mask = "../scenario-2/omit_data_mask.bmp"
-    env = VelocityControlledQuadcopter2DEnv(gating_bitmap=gating_bitmap)
+# def test_generate_transitions_dataset_const_action():
+#     num_states = 1000
+#     action = np.array([0.1, 0.1])
+#     # gating_bitmap = "../scenario-1/gating_mask.bmp"
+#     # omit_data_mask = "../scenario-1/omit_data_mask.bmp"
+#     gating_bitmap = "../scenario-2/gating_mask.bmp"
+#     omit_data_mask = "../scenario-2/omit_data_mask.bmp"
+#     env = VelocityControlledQuadcopter2DEnv(gating_bitmap=gating_bitmap)
 
-    (state_action_inputs, delta_state_outputs) = generate_transitions_dataset(
-        num_states=num_states,
-        num_actions=num_actions,
-        env=env,
-        omit_data_mask=omit_data_mask,
-    )
-    assert state_action_inputs.shape[0] == delta_state_outputs.shape[0]
-    assert state_action_inputs.shape[1] == 2 * delta_state_outputs.shape[1]
-    plt.quiver(
-        state_action_inputs[:, 0],
-        state_action_inputs[:, 1],
-        delta_state_outputs[:, 0],
-        delta_state_outputs[:, 1],
-    )
-    plt.show()
+#     (
+#         state_action_inputs,
+#         delta_state_outputs,
+#     ) = generate_transitions_dataset_const_action(
+#         action, num_states=num_states, env=env, omit_data_mask=omit_data_mask
+#     )
+#     assert state_action_inputs.shape[0] == delta_state_outputs.shape[0]
+#     assert state_action_inputs.shape[1] == 2 * delta_state_outputs.shape[1]
+#     plt.quiver(
+#         state_action_inputs[:, 0],
+#         state_action_inputs[:, 1],
+#         delta_state_outputs[:, 0],
+#         delta_state_outputs[:, 1],
+#     )
+#     plt.show()
 
 
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
+# def test_generate_transitions_dataset():
+#     num_states = 100
+#     num_actions = 16
+#     # gating_bitmap = "../scenario-1/gating_mask.bmp"
+#     # omit_data_mask = "../scenario-1/omit_data_mask.bmp"
+#     gating_bitmap = "../scenario-2/gating_mask.bmp"
+#     omit_data_mask = "../scenario-2/omit_data_mask.bmp"
+#     env = VelocityControlledQuadcopter2DEnv(gating_bitmap=gating_bitmap)
 
-    # test_generate_transitions_dataset_const_action()
-    test_generate_transitions_dataset()
+#     (state_action_inputs, delta_state_outputs) = generate_transitions_dataset(
+#         num_states=num_states,
+#         num_actions=num_actions,
+#         env=env,
+#         omit_data_mask=omit_data_mask,
+#     )
+#     assert state_action_inputs.shape[0] == delta_state_outputs.shape[0]
+#     assert state_action_inputs.shape[1] == 2 * delta_state_outputs.shape[1]
+#     plt.quiver(
+#         state_action_inputs[:, 0],
+#         state_action_inputs[:, 1],
+#         delta_state_outputs[:, 0],
+#         delta_state_outputs[:, 1],
+#     )
+#     plt.show()
+
+
+# if __name__ == "__main__":
+#     import matplotlib.pyplot as plt
+
+#     # test_generate_transitions_dataset_const_action()
+#     test_generate_transitions_dataset()
