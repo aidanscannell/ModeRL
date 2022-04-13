64e1c18c1b9c836bcd96907ab25872c4c70b96ff
Aidan Scannell <scannell.aidan@gmail.com>
2022-04-13 21:24:41 +0100

    deleted old scenario 4

--- examples/velocity_controlled_point_mass/scenario_4/superseded/learn_dynamics_from_config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from scenario_4.dynamics_learning.utils import train_dynamics
-
-
-if __name__ == "__main__":
-    # config_file = "./scenario_4/configs/initial_mogpe_config.toml"
-    config_file = "./scenario_4/configs/subset_mogpe_config.toml"
-
-    train_dynamics(config_file)
