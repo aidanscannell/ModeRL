52971f0200c2c3d253213cfeb4b6652cd3d9652c
Aidan Scannell <scannell.aidan@gmail.com>
2022-04-20 12:14:56 +0100

    commit of plotter

--- modeopt/plotting/contour_plotter.py
+++ modeopt/plotting/contour_plotter.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 from collections import OrderedDict
+
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from matplotlib import patches
 from modeopt.mode_opt import ModeOpt
+from modeopt.rollouts import (
+    collect_data_from_env,
+    rollout_controller_in_dynamics,
+    rollout_controller_in_env,
+)
 from modeopt.trajectories import GeodesicTrajectory
 from mogpe.keras.plotting import MixtureOfSVGPExpertsContourPlotter
 
@@ -15,7 +22,13 @@ MARKERS = {"env": "^", "dynamics": "."}
 LABELS = {"env": "Environment", "dynamics": "Dynamics", "collocation": "Collocation"}
 COLORS = {"env": "c", "dynamics": "m", "collocation": "y"}
 LINESTYLES = {"env": "-.", "dynamics": ":", "collocation": "-"}
-MARKERS = {"env": "^", "dynamics": ".", "collocation": "D"}
+MARKERS = {"env": "+", "dynamics": "o", "collocation": "x"}
+
+LINESTYLES = {"env": "-", "dynamics": "-", "collocation": "-"}
+MARKERS = {"env": "*", "dynamics": ".", "collocation": "d"}
+
+# matplotlib.rcParams.update({"font.size": 10})
+# matplotlib.rcParams.update({"font.size": 7})
 
 
 class ModeOptContourPlotter:
@@ -24,7 +37,14 @@ class ModeOptContourPlotter:
     Can handle arbitrary number of experts and output dimensions.
     """
 
-    def __init__(self, mode_optimiser: ModeOpt, test_inputs=None, static: bool = True):
+    def __init__(
+        self,
+        mode_optimiser: ModeOpt,
+        test_inputs=None,
+        static: bool = True,
+        static_trajectories: bool = False,
+        explorative: bool = False,  # whether to use explorative_controller or mode_controller
+    ):
         self.mode_optimiser = mode_optimiser
         self.mosvgpe_plotter = MixtureOfSVGPExpertsContourPlotter(
             mode_optimiser.dynamics.mosvgpe, test_inputs=test_inputs, static=static
@@ -32,37 +52,73 @@ class ModeOptContourPlotter:
         self.test_inputs = self.mosvgpe_plotter.test_inputs
         self.start_state = self.mode_optimiser.start_state
         self.target_state = self.mode_optimiser.target_state
+        self.explorative = explorative
 
         if not isinstance(self.test_inputs, tf.Tensor):
             self.test_inputs = tf.constant(self.test_inputs)
-        if isinstance(
-            self.mode_optimiser.mode_controller.previous_solution, GeodesicTrajectory
-        ):
-            metric = (
-                self.mode_optimiser.mode_controller.previous_solution.manifold.metric(
+        if self.mode_optimiser.mode_controller is not None:
+            if isinstance(
+                self.mode_optimiser.mode_controller.previous_solution,
+                GeodesicTrajectory,
+            ):
+                metric = self.mode_optimiser.mode_controller.previous_solution.manifold.metric(
                     self.test_inputs[:, 0 : self.mode_optimiser.dynamics.state_dim]
                 )
-            )
-            self.metric_trace = tf.linalg.trace(metric)
+                self.metric_trace = tf.linalg.trace(metric)
+
+        self.static_trajectories = static_trajectories
+        self.trajectories = self.generate_trajectories()
 
     def plot_model(self):
         self.plot_trajectories_over_gating_network_gps()
+        self.plot_trajectories_over_desired_gating_network_gp()
         self.plot_trajectories_over_mixing_probs()
 
-    def plot_trajectories_over_gating_network_gps(self):
+    def plot_trajectories_over_gating_network_gps(
+        self, plot_satisfaction_prob: bool = False
+    ):
         fig = self.mosvgpe_plotter.plot_gating_network_gps()
         fig = self.plot_env_given_fig(fig)
-        self.plot_trajectories_given_fig(fig)
+        self.plot_trajectories_given_fig(
+            fig, plot_satisfaction_prob=plot_satisfaction_prob
+        )
         return fig
 
-    def plot_trajectories_over_mixing_probs(self):
+    def plot_trajectories_over_desired_gating_network_gp(
+        self, plot_satisfaction_prob: bool = False
+    ):
+        fig = self.mosvgpe_plotter.plot_single_gating_network_gp(
+            desired_mode=self.mode_optimiser.desired_mode
+        )
+        fig = self.plot_env_given_fig(fig)
+        self.plot_trajectories_given_fig(
+            fig, plot_satisfaction_prob=plot_satisfaction_prob
+        )
+        return fig
+
+    def plot_trajectories_over_mixing_probs(self, plot_satisfaction_prob: bool = False):
         fig = self.mosvgpe_plotter.plot_mixing_probs()
         fig = self.plot_env_given_fig(fig)
-        self.plot_trajectories_given_fig(fig)
+        self.plot_trajectories_given_fig(
+            fig, plot_satisfaction_prob=plot_satisfaction_prob
+        )
+        return fig
+
+    def plot_trajectories_over_desired_mixing_prob(
+        self, plot_satisfaction_prob: bool = False
+    ):
+        fig = self.mosvgpe_plotter.plot_single_mixing_prob(
+            self.mode_optimiser.desired_mode
+        )
+        fig = self.plot_env_given_fig(fig)
+        self.plot_trajectories_given_fig(
+            fig, plot_satisfaction_prob=plot_satisfaction_prob
+        )
         return fig
 
     def plot_trajectories_over_metric_trace(self):
-        fig = plt.figure(figsize=(self.mosvgpe_plotter.figsize))
+        figsize = self.mosvgpe_plotter.figsize
+        fig = plt.figure(figsize=(figsize[0] / 2, figsize[1]))
         gs = fig.add_gridspec(1, 1)
         ax = gs.subplots()
         # fig.suptitle("Trace of expected metric $\\text{tr}(\mathbb{E}[G(\mathbf{x})])$")
@@ -71,21 +127,81 @@ class ModeOptContourPlotter:
         self.plot_trajectories_given_fig(fig)
         return fig
 
-    def plot_trajectories_given_fig(self, fig):
-        trajectories = self.generate_trajectories()
-        for key in trajectories.keys():
-            self.plot_trajectory_given_fig(fig, trajectories[key], key)
-        # fig.legend(loc="lower center", bbox_transform=fig.transFigure)
-        handles, labels = plt.gca().get_legend_handles_labels()
-        by_label = OrderedDict(zip(labels, handles))
-        fig.legend(
-            by_label.values(),
-            by_label.keys(),
-            loc="upper center",
-            # loc="lower center",
-            bbox_transform=fig.transFigure,
-            ncol=len(by_label),
+    def plot_data_over_desired_gating_network_gp(self):
+        fig = self.mosvgpe_plotter.plot_single_gating_network_gp(
+            desired_mode=self.mode_optimiser.desired_mode
         )
+        fig = self.plot_env_given_fig(fig)
+        self.plot_data_given_fig(fig)
+        return fig
+
+    def plot_data_over_mixing_probs(self):
+        fig = self.mosvgpe_plotter.plot_mixing_probs()
+        fig = self.plot_env_given_fig(fig)
+        self.plot_data_given_fig(fig)
+        return fig
+
+    def plot_data_given_fig(self, fig):
+        axs = fig.get_axes()
+        states = self.mode_optimiser.dataset[0]
+        mixing_probs = (
+            self.mode_optimiser.dynamics.mosvgpe.gating_network.predict_mixing_probs(
+                self.test_inputs
+            )
+        )
+        for ax in axs:
+            # self.plot_start_end_pos_given_ax(ax)
+            # ax.set_xlabel("$x$")
+            # ax.set_ylabel("$y$")
+            ax.scatter(
+                states[:, 0],
+                states[:, 1],
+                marker="x",
+                color="b",
+                linewidth=0.5,
+                alpha=0.5,
+                label="Observations",
+            )
+            CS = ax.tricontour(
+                self.test_inputs[:, 0],
+                self.test_inputs[:, 1],
+                mixing_probs[:, self.mode_optimiser.desired_mode].numpy(),
+                [self.mode_optimiser.mode_satisfaction_probability],
+            )
+            ax.clabel(CS, inline=True, fontsize=10)
+        self.plot_env_no_obs_start_end_given_fig(fig)
+
+    def plot_trajectories_given_fig(self, fig, plot_satisfaction_prob: bool = False):
+        if not self.static_trajectories:
+            self.trajectories = self.generate_trajectories()
+        for key in self.trajectories.keys():
+            self.plot_trajectory_given_fig(fig, self.trajectories[key], key)
+        self.plot_env_no_obs_start_end_given_fig(fig)
+        if plot_satisfaction_prob:
+            mixing_probs = self.mode_optimiser.dynamics.mosvgpe.gating_network.predict_mixing_probs(
+                self.test_inputs
+            )
+            for ax in fig.get_axes():
+                CS = ax.tricontour(
+                    self.test_inputs[:, 0],
+                    self.test_inputs[:, 1],
+                    mixing_probs[:, self.mode_optimiser.desired_mode].numpy(),
+                    [self.mode_optimiser.mode_satisfaction_probability],
+                )
+                ax.clabel(CS, inline=True, fontsize=10)
+        # fig.legend(loc="lower center", bbox_transform=fig.transFigure)
+        # handles, labels = plt.gca().get_legend_handles_labels()
+        # by_label = OrderedDict(zip(labels, handles))
+        # fig.legend(
+        #     by_label.values(),
+        #     by_label.keys(),
+        #     # loc="upper center",
+        #     bbox_to_anchor=(0.5, 0.05),
+        #     # loc="lower center",
+        #     loc="upper center",
+        #     bbox_transform=fig.transFigure,
+        #     ncol=len(by_label),
+        # )
         # fig.legend(loc="lower center", bbox_transform=fig.transFigure, ncol=8)
 
     # def map_over_axs(fig):
@@ -131,16 +247,22 @@ class ModeOptContourPlotter:
         # self.plot_no_observations_given_fig_ax(fig, ax)
 
     def plot_env(self):
-        fig = plt.figure(figsize=(self.mosvgpe_plotter.figsize))
-        gs = fig.add_gridspec(1, 1, wspace=0.3)
+        figsize = self.mosvgpe_plotter.figsize
+        fig = plt.figure(figsize=(figsize[0] / 2, figsize[1]))
+        gs = fig.add_gridspec(1, 1)
         ax = gs.subplots()
-        fig.suptitle("Environment gating mask")
-        test_states = self.test_inputs[:, 0 : self.mode_opt.dynamics.state_dim]
-        mode_probs = []
-        for test_state in test_states:
-            mode_probs.append(self.mode_optimiser.env.state_to_pixel(test_states))
-        self.plot_contf(fig, ax, mode_probs)
-        self.plot_start_end_pos_given_ax(ax)
+        if self.mode_optimiser.dataset is not None:
+            ax.quiver(
+                self.mode_optimiser.dataset[0][:, 0],
+                self.mode_optimiser.dataset[0][:, 1],
+                self.mode_optimiser.dataset[1][:, 0],
+                self.mode_optimiser.dataset[1][:, 1],
+                alpha=0.5,
+            )
+        self.plot_env_given_fig(fig)
+        self.plot_start_end_pos_given_ax(ax, bbox=True)
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$y$")
         return fig
 
     def plot_env_given_fig(self, fig):
@@ -164,12 +286,37 @@ class ModeOptContourPlotter:
             mode_probs.numpy(),
             [0.5],
         )
-        ax.clabel(CS, inline=True, fontsize=10, fmt={0.5: "Boundary"})
-        # ax.clabel(CS, inline=True, fontsize=10)
-        # CS.collections[0].set_label("Mode boundary")
-        # ax.clabel(CS, inline=True, fontsize=10, fmt={0.5: "Mode boundary"})
+        try:
+            # clabel = ax.clabel(CS, inline=True, fmt={0.5: "Mode boundary"})
+            clabel = ax.clabel(CS, inline=True, fontsize=8, fmt={0.5: "Mode boundary"})
+            clabel[0].set_bbox(dict(boxstyle="round,pad=0.1", fc="white", alpha=1.0))
+        except IndexError:
+            pass
 
-    def plot_start_end_pos_given_ax(self, ax):
+    def plot_start_end_pos_given_ax(self, ax, bbox=False):
+        if bbox:
+            bbox = dict(boxstyle="round,pad=0.1", fc="thistle", alpha=1.0)
+        else:
+            bbox = None
+        if len(self.start_state.shape) == 1:
+            self.start_state = self.start_state[tf.newaxis, :]
+        if len(self.target_state.shape) == 1:
+            self.target_state = self.target_state[tf.newaxis, :]
+        ax.annotate(
+            "$\mathbf{x}_0$",
+            (self.start_state[0, 0] + 0.1, self.start_state[0, 1]),
+            horizontalalignment="left",
+            verticalalignment="top",
+            bbox=bbox,
+        )
+        ax.annotate(
+            "$\mathbf{x}_f$",
+            (self.target_state[0, 0] - 0.1, self.target_state[0, 1]),
+            horizontalalignment="right",
+            verticalalignment="bottom",
+            bbox=bbox,
+            # bbox=dict(boxstyle="round,pad=0.1", fc="thistle", alpha=1.0),
+        )
         ax.scatter(
             self.start_state[0, 0], self.start_state[0, 1], marker="x", color="k", s=8.0
         )
@@ -180,40 +327,102 @@ class ModeOptContourPlotter:
             marker="x",
             s=8.0,
         )
-        ax.annotate(
-            "$\mathbf{x}_0$",
-            (self.start_state[0, 0] + 0.05, self.start_state[0, 1]),
-            horizontalalignment="left",
-            verticalalignment="top",
-        )
-        ax.annotate(
-            "$\mathbf{x}_f$",
-            (self.target_state[0, 0] - 0.05, self.target_state[0, 1]),
-            horizontalalignment="right",
-            verticalalignment="bottom",
-        )
 
     def generate_trajectories(self):
-        if isinstance(
-            # self.mode_optimiser.mode_controller.previous_solution,
-            self.mode_optimiser.mode_controller.previous_solution,
-            GeodesicTrajectory,
-        ):
-            print("is instance of GeodesicTrajectory")
-            # collocation_trajectory = {
-            #     "collocation": self.mode_optimiser.mode_controller.previous_solution.states
-            # }
-            # trajectories = collocation_trajectory
-            dynamics_trajectory = self.mode_optimiser.dynamics_rollout()[0]
-            env_trajectory = self.mode_optimiser.env_rollout()
-            trajectories = {
-                "env": env_trajectory,
-                "dynamics": dynamics_trajectory,
-                "collocation": self.mode_optimiser.mode_controller.previous_solution.states,
-            }
+        if self.explorative:
+            dynamics_trajectory, _ = rollout_controller_in_dynamics(
+                dynamics=self.mode_optimiser.dynamics,
+                controller=self.mode_optimiser.explorative_controller,
+                start_state=self.mode_optimiser.start_state.numpy(),
+            )
+            trajectories = {"dynamics": dynamics_trajectory}
+            env_trajectory = rollout_controller_in_env(
+                env=self.mode_optimiser.env,
+                controller=self.mode_optimiser.explorative_controller,
+                start_state=self.mode_optimiser.start_state.numpy(),
+            )
+            trajectories.update({"env": env_trajectory})
         else:
-            print("is NOT instance of GeodesicTrajectory")
+
             dynamics_trajectory = self.mode_optimiser.dynamics_rollout()[0]
-            env_trajectory = self.mode_optimiser.env_rollout()
-            trajectories = {"env": env_trajectory, "dynamics": dynamics_trajectory}
+            trajectories = {"dynamics": dynamics_trajectory}
+            if self.mode_optimiser.env is not None:
+                trajectories.update({"env": self.mode_optimiser.env_rollout()})
+            if isinstance(
+                self.mode_optimiser.mode_controller.previous_solution,
+                GeodesicTrajectory,
+            ):
+                print("is instance of GeodesicTrajectory")
+                trajectories.update(
+                    {
+                        "collocation": self.mode_optimiser.mode_controller.previous_solution.states
+                    }
+                )
+            else:
+                print("is NOT instance of GeodesicTrajectory")
+                # dynamics_trajectory = self.mode_optimiser.dynamics_rollout()[0]
+                # env_trajectory = self.mode_optimiser.env_rollout()
+                # trajectories = {"env": env_trajectory, "dynamics": dynamics_trajectory}
         return trajectories
+
+    def plot_env_no_obs_start_end_given_fig(self, fig, trim_coords=None):
+        self.plot_env_given_fig(fig)
+        axs = fig.get_axes()
+        for ax in axs:
+            self.plot_no_observations_given_ax(ax, trim_coords=trim_coords)
+            self.plot_start_end_pos_given_ax(ax, bbox=True)
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = OrderedDict(zip(labels, handles))
+        fig.legend(
+            by_label.values(),
+            by_label.keys(),
+            bbox_to_anchor=(0.5, 0.05),
+            loc="upper center",
+            bbox_transform=fig.transFigure,
+            ncol=len(by_label),
+        )
+
+    def plot_no_observations_given_ax(self, ax, trim_coords=None):
+        color = "k"
+        x_min = self.mode_optimiser.env.observation_spec().minimum[0]
+        y_min = self.mode_optimiser.env.observation_spec().minimum[1]
+        x_max = self.mode_optimiser.env.observation_spec().maximum[0]
+        y_max = self.mode_optimiser.env.observation_spec().maximum[1]
+        # y_max = 3
+        # y_min = -3
+        ax.add_patch(
+            patches.Rectangle(
+                (x_min, y_min),
+                x_max - x_min,
+                y_max - y_min,
+                edgecolor=color,
+                facecolor=color,
+                # hatch="///",
+                # label="Environment boundary",
+                # label="No observations",
+                fill=False,
+                ls="--",
+            )
+        )
+        if trim_coords:
+            if trim_coords[0][0] < x_min:
+                trim_coords[0][0] = x_min
+            if trim_coords[0][1] < y_min:
+                trim_coords[0][1] = y_min
+            if trim_coords[1][0] > x_max:
+                trim_coords[1][0] = x_max
+            if trim_coords[1][1] > y_max:
+                trim_coords[1][1] = y_max
+            ax.add_patch(
+                patches.Rectangle(
+                    (trim_coords[0][0], trim_coords[0][1]),
+                    trim_coords[1][0] - trim_coords[0][0],
+                    trim_coords[1][1] - trim_coords[0][1],
+                    edgecolor=color,
+                    facecolor=color,
+                    hatch="///",
+                    label="No observations",
+                    fill=False,
+                    linewidth=0,
+                )
+            )
