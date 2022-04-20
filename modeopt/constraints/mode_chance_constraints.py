63399cc96da7edc4f1342e088def298cca5f1d6c
Aidan Scannell <scannell.aidan@gmail.com>
2022-04-20 11:50:18 +0100

    updated mode chance constraints

--- modeopt/constraints/mode_chance_constraints.py
+++ modeopt/constraints/mode_chance_constraints.py
@@ -15,9 +15,10 @@ def build_mode_chance_constraints_fn(
     mode_opt_dynamics: ModeOptDynamics,
     start_state: ttf.Tensor2[Batch, StateDim],
     horizon: int = 10,
+    control_dim: int = 1,
     compile: bool = True,
 ) -> Callable:
-    control_dim = mode_opt_dynamics.control_dim
+    # control_dim = mode_opt_dynamics.control_dim
 
     def mode_chance_constraints(controls_flat: tf.Tensor) -> tf.Tensor:
         if controls_flat.shape[0] == horizon * control_dim:
@@ -37,15 +38,17 @@ def build_mode_chance_constraints_fn(
             control_vars=control_vars,
         )
         mode_prob = mode_opt_dynamics.predict_mode_probability(
-            state_means[:-1, :],
+            # state_means[:-1, :],
+            state_means[1:, :],
             control_means,
-            state_vars[:-1, :],
+            # state_vars[:-1, :],
+            state_vars[1:, :],
             control_vars
             # state_means[1:, :], control_means, state_vars[1:, :], control_vars
         )
         mode_prob_flat = tf.reshape(mode_prob, [-1])
-        tf.print("mode_prob_flat")
-        tf.print(mode_prob_flat)
+        # tf.print("mode_prob_flat")
+        # tf.print(mode_prob_flat)
         if controls_flat.shape[0] == horizon * control_dim * 2:
             var_probs = tf.ones(mode_prob_flat.shape, dtype=default_float())
             return tf.concat([mode_prob_flat, var_probs], axis=0)
@@ -62,11 +65,12 @@ def build_mode_chance_constraints_scipy(
     mode_opt_dynamics: ModeOptDynamics,
     start_state: ttf.Tensor2[Batch, StateDim],
     horizon: int = 10,
+    control_dim: int = 1,
     lower_bound: float = 0.5,
     upper_bound: float = 1.0,
     compile: bool = True,
 ) -> NonlinearConstraint:
     constraints_fn = build_mode_chance_constraints_fn(
-        mode_opt_dynamics, start_state=start_state, horizon=horizon, compile=compile
+        mode_opt_dynamics, start_state=start_state, horizon=horizon, control_dim=control_dim, compile=compile
     )
     return NonlinearConstraint(constraints_fn, lower_bound, upper_bound)
