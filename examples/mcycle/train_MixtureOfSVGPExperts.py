303a2d73e6da3b06fbabb9d095ca2375f0be282e
Aidan Scannell <scannell.aidan@gmail.com>
2022-09-07 09:48:03 +0300

    added functionality for 3 experts

--- examples/mcycle/train_MixtureOfSVGPExperts.py
+++ examples/mcycle/train_MixtureOfSVGPExperts.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from gpflow import default_float
-from gpflow.inducing_variables import InducingPoints
+from gpflow.inducing_variables import InducingPoints, MultioutputInducingVariables
 from gpflow.kernels import RBF
 from gpflow.likelihoods import Gaussian
 from gpflow.mean_functions import Constant
@@ -23,7 +23,7 @@ import wandb
 
 PlotFn = Callable[[], matplotlib.figure.Figure]
 
-colors = ["m", "c"]
+colors = ["m", "c", "y"]
 
 tf.random.set_seed(42)
 np.random.seed(42)
@@ -56,14 +56,15 @@ def build_SVGPExpert(
 
 def build_SVGPGatingNetwork(
     X: InputData,
-    lengthscales: List[float] = [1.0],
+    # lengthscales: List[float] = [1.0],
+    lengthscales: float = 1.0,
     kernel_variance: float = 1.0,
     num_gating_gps: int = 1,
 ) -> SVGPGatingNetwork:
+    idx = np.random.choice(range(X.shape[0]), size=num_inducing, replace=False)
+    Z = X.numpy()[idx, ...].reshape(-1, X.shape[1])
     if num_gating_gps == 1:
         kernel = RBF(lengthscales=lengthscales, variance=kernel_variance)
-        idx = np.random.choice(range(X.shape[0]), size=num_inducing, replace=False)
-        Z = X.numpy()[idx, ...].reshape(-1, X.shape[1])
         inducing_variable = InducingPoints(Z)
     elif num_gating_gps > 1:
         kernels = [
@@ -71,8 +72,8 @@ def build_SVGPGatingNetwork(
             for _ in range(num_gating_gps)
         ]
         kernel = gpf.kernels.SeparateIndependent(kernels=kernels)
-        raise NotImplementedError(
-            "need to implement method for instantiating multioutput inducing variables"
+        inducing_variable = gpf.inducing_variables.SharedIndependentInducingVariables(
+            gpf.inducing_variables.InducingPoints(Z)
         )
     else:
         raise AttributeError("num_gating_gps should be >= 1")
@@ -163,9 +164,14 @@ def plot_gating_networks_gps():
     gs = fig.add_gridspec(1, 1)
     ax = gs.subplots()
     mean, var = model.gating_network.predict_h(test_inputs)
+    if isinstance(
+        model.gating_network.gp.inducing_variable, MultioutputInducingVariables
+    ):
+        Z = model.gating_network.gp.inducing_variable.inducing_variable.Z
+    else:
+        Z = model.gating_network.gp.inducing_variable.Z
     for k in range(mean.shape[-1]):
         plot_gp(ax, mean[:, k], var[:, k], label="k=" + str(k + 1), color=colors[k])
-        Z = model.gating_network.gp.inducing_variable.Z
         ax.scatter(Z, np.zeros(Z.shape), marker="|", color=colors[k])
     ax.legend()
     return fig
@@ -182,7 +188,7 @@ def plot_mixing_probs():
     return fig
 
 
-def plot_posterior_samples():
+def plot_posterior():
     fig = plt.figure()
     gs = fig.add_gridspec(1, 1)
     ax = gs.subplots()
@@ -196,6 +202,8 @@ def plot_posterior_samples():
     )  # [S, N]
     alpha_samples = tf.expand_dims(alpha_samples, -1)  # [S, N, 1]
     experts_dists = model.predict_experts_dists(test_inputs)
+    y_mean = model.predict_y(test_inputs).mean()
+    ax.plot(test_inputs, y_mean, color="k", lw=2, label="Posterior mean")
     for k in range(model.num_experts):
         y_samples = experts_dists[k].sample(num_samples)
         ax.scatter(
@@ -203,7 +211,6 @@ def plot_posterior_samples():
             y_samples[alpha_samples == k],
             c=colors[k],
             s=3,
-            # cmap=cmap,
             alpha=0.8,
             label="k=" + str(k + 1) + " samples",
         )
@@ -225,13 +232,26 @@ def plot_gp(ax, mean, var, label="", color="C0", alpha=0.4):
 
 if __name__ == "__main__":
     mcylce_dataset_filename = "./mcycle.csv"
-    num_inducing = 32
+
+    # Two expert model config
     num_experts = 2
     num_gating_gps = 1
     experts_lengthscales = [1.0, 10.0]
     experts_kernel_variances = [1.0, 1.0]
 
-    batch_size = 16
+    # Three expert model config
+    num_experts = 3
+    num_gating_gps = 3
+    experts_lengthscales = [1.0, 10.0, 1.0]
+    experts_kernel_variances = [1.0, 1.0, 1.0]
+
+    # General model config
+    num_inducing = 32
+
+    # Training config
+    # batch_size = 16
+    # batch_size = 32
+    batch_size = 64
     epochs = 10000
     # epochs = 25000
     num_samples = 1
@@ -262,12 +282,13 @@ if __name__ == "__main__":
     # Build model and sample GP inducing variables from X
     experts_list = []
     for k in range(num_experts):
-        expert = build_SVGPExpert(
-            X=X,
-            lengthscales=experts_lengthscales[k],
-            kernel_variance=experts_kernel_variances[k],
+        experts_list.append(
+            build_SVGPExpert(
+                X=X,
+                lengthscales=experts_lengthscales[k],
+                kernel_variance=experts_kernel_variances[k],
+            )
         )
-        experts_list.append(expert)
     gating_network = build_SVGPGatingNetwork(X=X, num_gating_gps=num_gating_gps)
     model = MixtureOfSVGPExperts(
         experts_list=experts_list, gating_network=gating_network
@@ -301,9 +322,9 @@ if __name__ == "__main__":
             name="Mixing Probs",
         ),
         PlottingCallback(
-            plot_posterior_samples,
+            plot_posterior,
             logging_epoch_freq=logging_epoch_freq,
-            name="Posterior Samples",
+            name="Posterior",
         ),
     ]
 
