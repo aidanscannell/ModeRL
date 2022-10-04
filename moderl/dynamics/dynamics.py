4bd1d2916c9e6aae4f7bebba2228128c1838e410
Aidan Scannell <scannell.aidan@gmail.com>
2022-10-04 19:24:46 +0300

    set batch_size=num_data after updating data set

--- moderl/dynamics/dynamics.py
+++ moderl/dynamics/dynamics.py
@@ -129,6 +129,8 @@ class ModeRLDynamics(DynamicsInterface):
         else:
             self.dataset = dataset
         self.mosvgpe.num_data = self.dataset[0].shape[0]
+        # TODO should I really be updating batch size here?
+        self.dynamics_fit_kwargs.update({"batch_size": self.dataset[0].shape[0]})
 
     def predict_mode_probability(
         self,
