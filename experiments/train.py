a043d5e5030ab0b79d4a09a2b3cf72b670327f3e
Aidan Scannell <scannell.aidan@gmail.com>
2022-11-28 17:15:21 +0200

    changed .plot to plot

--- experiments/train.py
+++ experiments/train.py
@@ -17,17 +17,16 @@ from moderl.controllers import ExplorativeController
 from moderl.custom_types import State
 from moderl.dynamics import ModeRLDynamics
 from moderl.rollouts import collect_data_from_env
+from plot.callbacks.controller import (
+    plot_trajectories_over_desired_gating_gp,
+    plot_trajectories_over_desired_mixing_prob,
+)
 
 # from experiments.plot.utils import create_test_inputs
 # from experiments.utils import sample_mosvgpe_inducing_inputs_from_data
 from plot.utils import create_test_inputs
 from utils import sample_mosvgpe_inducing_inputs_from_data
 from wandb.keras import WandbCallback
-
-from .plot.callbacks.controller import (
-    plot_trajectories_over_desired_gating_gp,
-    plot_trajectories_over_desired_mixing_prob,
-)
 
 
 # from experiments.plot.callbacks import build_dynamics_plotting_callbacks
