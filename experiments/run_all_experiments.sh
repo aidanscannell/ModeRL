5552d15b734cfa0463668a766bd003916b6479a7
Aidan Scannell <scannell.aidan@gmail.com>
2022-11-29 10:24:38 +0200

    updated how arguments are passes to run_epxeriment.slrm

--- experiments/run_all_experiments.sh
+++ experiments/run_all_experiments.sh
@@ -2,5 +2,5 @@
 
 for path in configs/experiment/*.yaml; do  # loop through experiments
 	echo "Running: python train.py +experiment=$(basename ${path} .yaml)"
-	sbatch run_experiment.slrm +experiment=$(basename ${path} .yaml)
+	sbatch run_experiment.slrm +experiment=$(basename ${path} .yaml) ++controller.exploration_weight=1
 done
