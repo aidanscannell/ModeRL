18032f46ba6205700a108bac499d230c6a328ab0
Aidan Scannell <scannell.aidan@gmail.com>
2022-11-29 11:12:24 +0200

    made beta=50

--- experiments/run_all_experiments.sh
+++ experiments/run_all_experiments.sh
@@ -2,5 +2,5 @@
 
 for path in configs/experiment/*.yaml; do  # loop through experiments
 	echo "Running: python train.py +experiment=$(basename ${path} .yaml)"
-	sbatch run_experiment.slrm "+experiment=$(basename ${path} .yaml) ++controller.exploration_weight=5"
+	sbatch run_experiment.slrm "+experiment=$(basename ${path} .yaml) ++controller.exploration_weight=20"
 done
